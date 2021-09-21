04b205820e53ef7e7633ef6a7607a0cc44b7ff2b
Mabel <maybelchristina@gmail.com>
2021-09-21 14:47:55 +0530

    [Mabel] Add: added UC1

---
+++ BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+   
+    class FlipCoin
+    {
+        int head = 0, tail = 0, headpercentage, tailpercentage;
+        public void Flip(int N)
+        {
+            for(int i=0;i<N;i++)
+            {
+                Random random = new Random();
+                if (random.NextDouble() > 0.5)
+                    this.head++;
+                else
+                    this.tail++;
+                N--;
+            }
+            this.headpercentage = (this.head / N) * 100;
+            this.tailpercentage = (this.tail / N) * 100;
+            Console.WriteLine("Percentage of getting head " + headpercentage);
+            Console.WriteLine("Percentage of getting tail " + tailpercentage);
+        }
+    }
+}
---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.FlipCoin \n");
+            int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.Flip(Convert.ToInt32(Console.ReadLine()));
+                    break;
+            }
+
+        }
+    }
+}
