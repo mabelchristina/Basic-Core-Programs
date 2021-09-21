d32f7ea77391cead36cafae05b89be706604b074
Mabel <maybelchristina@gmail.com>
2021-09-21 15:46:03 +0530

    [Mabel] Add: UC2 added

---
+++ BasicCorePrograms/Leap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class Leap
+    {
+        public void LeapYear(int year)
+        {
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+                Console.WriteLine(year+" is Leap Year");
+            else
+                Console.WriteLine(year +" is not a Leap Year");
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.FlipCoin \n");
+            Console.WriteLine("1.FlipCoin \n2.LeapYear \n");
             int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
             {
@@ -14,9 +14,11 @@ namespace BasicCorePrograms
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.Flip(Convert.ToInt32(Console.ReadLine()));
                     break;
-                //case 2:
-                 //   FlipCoin flipCoin1 = new FlipCoin();
-                  //  fl
+                case 2:
+                    Leap leapyear = new Leap();
+                    leapyear.LeapYear(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
             }
 
         }
